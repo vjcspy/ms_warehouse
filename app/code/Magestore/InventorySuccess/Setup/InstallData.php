f8dead5d720abb07114b894f4dda22d960a187c6
vjcspy <mr.vjcspy@gmail.com>
2017-04-04 14:48:33 +0700

    - install table

--- app/code/Magestore/InventorySuccess/Setup/InstallData.php
+++ app/code/Magestore/InventorySuccess/Setup/InstallData.php
@@ -13,8 +13,8 @@ use Magento\Framework\Setup\ModuleDataSetupInterface;
 /**
  * @codeCoverageIgnore
  */
-class InstallData implements InstallDataInterface
-{
+class InstallData implements InstallDataInterface {
+
     /**
      *
      * @var \Magestore\InventorySuccess\Api\InstallManagementInterface
@@ -26,27 +26,36 @@ class InstallData implements InstallDataInterface
      * @param \Magestore\InventorySuccess\Api\InstallManagementInterface $installManagement
      */
     public function __construct(
-    \Magestore\InventorySuccess\Api\InstallManagementInterface $installManagement
-    )
-    {
+        \Magestore\InventorySuccess\Api\InstallManagementInterface $installManagement
+    ) {
         $this->_installManagement = $installManagement;
     }
 
     /**
      * @param ModuleDataSetupInterface $setup
-     * @param ModuleContextInterface $context
+     * @param ModuleContextInterface   $context
      */
-    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
-    {
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context) {
         $setup->startSetup();
 
         /* create default warehouse */
+        /*
+         * Cái này làm 2 việc:
+         * 1. tạo 1 row trong os_warehouse là default warehouse
+         * 2. tạo thêm 1 stock trong bảng cataloginventory_stock nhưng khó hiểu là sao nó lại lưu warehouse_id vào cột website_id
+         * */
         $this->_installManagement->createDefaultWarehouse();
 
         /* transfer products to default warehouse */
+        /*
+         * Bọn này dùng field website_id để làm warehouse_id, nó duplicate các row có sẵn và chuyển website_id thành warehouse_id primary
+         * */
         $this->_installManagement->transferProductsToDefaultWarehouse();
 
         /* calculate qty-to-ship of all products, save to database, update to default warehouse */
+        /*
+         * Để xem là order sẽ bị trừ vào warehouse nào
+         * */
         $this->_installManagement->calculateQtyToShip();
 
         /** create default notification rule */
